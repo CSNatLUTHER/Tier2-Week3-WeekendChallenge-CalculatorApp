a6d2cfd07629380dd9d5ca75f97ca9cc9e3cc5d9
Chris Nelson <cnelson04@gmail.com>
2021-09-23 13:46:21 -0500

    GET on server and client working - displays calculation history

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,5 +2,20 @@ $( document ).ready ( onReady );
 
 function onReady(){
     console.log( 'I am JQuery ready!' );
+    getPastCalculations();
 }
 
+function getPastCalculations(){
+    console.log('in getPastCalculations');
+    $.ajax({
+        method: 'GET',
+        url: '/calculations'
+    }).then( function( response ) {
+        console.log('Back from server with:', response );
+        let el = $("#listOfPastCalculations")
+        for (let i = 0; i < response.length; i++) {
+            el.append(`<li>${response[i].numOne} ${response[i].operation} ${response[i].numTwo} = <b>${response[i].answer}</b></li>`)
+            
+        }; // end for loop
+    })
+}
--- server/server.js
+++ server/server.js
@@ -10,9 +10,19 @@
 
 // GLOBAL VIARIABLES
     const port = 5000;
+    let pastCalculations = [{
+        numOne: 5,
+        operation: '*',
+        numTwo: 7,
+        answer: 35
+    }];
 
 // SPIN UP SERVER
     app.listen( port, () => {
         console.log( 'server is up on:', port );
     })
 // ROUTES
+app.get('/calculations', (req, res ) => {
+ console.log( 'hit on GET /calculations');
+ res.send( pastCalculations );
+});
